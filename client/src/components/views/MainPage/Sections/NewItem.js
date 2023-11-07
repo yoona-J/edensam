8fd0355dda5d7a7d5a874bc071ea7fce827e3d4f
hyunseo134 <hyunseo134@gmail.com>
2023-11-08 06:35:28 +0900

    minor edit

--- client/src/components/views/MainPage/Sections/NewItem.js
+++ client/src/components/views/MainPage/Sections/NewItem.js
@@ -32,72 +32,63 @@ function NewItem(props) {
             }}
           />
 
-          <div style={{width:"100px",textAlign:"left"}}>{upload.item_title}</div>
-          <div style={{width:"100px",textAlign:"right"}}>{`${upload.how_much}원`}</div>
+          <div style={{ width: "100px", textAlign: "left" }}>
+            {upload.item_title}
+          </div>
+          {/* <div style={{width:"100px",textAlign:"right"}}>{`${upload.how_much}원`}</div> */}
         </div>
       </a>
     );
   });
 
   return (
-    // <div
-    //   style={{
-    //     display: "flex",
-    //     justifyContent: "space-around",
-    //     flexWrap: "wrap",
-    //   }}
-    // >
-    //   <div>{renderCards[7]}</div>
-    //   <div>{renderCards[13]}</div>
-    //   <div>{renderCards[18]}</div>
-    // </div>
     <div>
-  <div>
-    <p
-      style={{
-        fontSize: "20px",
-        textAlign: "center",
-        fontFamily: "Pretendard-Medium",
-      }}
-    >
-      따끈따끈한 신상
-    </p>
-  </div>
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "space-around",
-      flexWrap: "wrap",
-    }}
-  >
-    <div>{renderCards[7]}</div>
-    <div>{renderCards[13]}</div>
-    <div>{renderCards[18]}</div>
-  </div>
-  <hr />
-  <div>
-    <p
-      style={{
-        fontSize: "20px",
-        textAlign: "center",
-        fontFamily: "Pretendard-Medium",
-      }}
-    >
-      회원님이 좋아할만한 선물
-    </p>
-  </div>
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "space-around",
-      flexWrap: "wrap",
-    }}
-  >
-    <div>{renderCards[12]}</div>
-    <div>{renderCards[13]}</div>
-    <div>{renderCards[15]}</div>
-  </div>
-</div>
+      <div>
+        <p
+          style={{
+            fontSize: "20px",
+            textAlign: "center",
+            fontFamily: "Pretendard-Medium",
+          }}
+        >
+          따끈따끈한 신상
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+        }}
+      >
+        <div>{renderCards[7]}</div>
+        <div>{renderCards[13]}</div>
+        <div>{renderCards[18]}</div>
+      </div>
+      <hr />
+      <div>
+        <p
+          style={{
+            fontSize: "20px",
+            textAlign: "center",
+            fontFamily: "Pretendard-Medium",
+          }}
+        >
+          회원님이 좋아할만한 선물
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+        }}
+      >
+        <div>{renderCards[12]}</div>
+        <div>{renderCards[14]}</div>
+        <div>{renderCards[15]}</div>
+      </div>
+    </div>
   );
 }
 
